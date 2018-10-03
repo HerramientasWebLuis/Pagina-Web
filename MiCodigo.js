f788101c7cbb7fd8427567a3ffb623fc7faa6e2e
HerramientasWebLuis <luisbonfanti.17@gmail.com>
2018-10-03 02:42:41 -0300

    mi primer proyecto

---
+++ MiCodigo.js
@@ -0,0 +1,12 @@
+function Bienvenida(){
+         alert("Bienvenido a ArtMedia! Donde encontraras todo para aprender y mejorar tu dibujo");
+}
+
+function Mensaje() {
+	     document.write("<h1>¡Aún estamos trabajando en la sección sobre Realidad Virtual, agradecemos su paciencia!</h1>");
+}
+
+function Suerte(){
+         alert("¡Buena suerte y diviértete aprendiendo, no pierdas el ánimo!");
+}
+
